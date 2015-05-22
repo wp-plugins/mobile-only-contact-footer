2b93f6d13f4afa09109841f14e82c562b1ece023
ExpoExe <ExpoExe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-22 21:51:23 +0000

    Trying to implement the Iris color picker.
    git-svn-id: https://plugins.svn.wordpress.org/mobile-only-contact-footer/trunk@1165999 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mcf-script.js
+++ mcf-script.js
@@ -1,35 +1,5 @@
-jQuery(document).ready(function($) {
-    $('.mcf-color-picker1').hide();
-	$('.mcf-color-picker2').hide();
-    $('#mcf_section_bg_color').wpColorPicker();
-	$('#mcf_section_icon_color').wpColorPicker();
-
-    $('#mcf_section_bg_color').click(function() {
-		if(!$('.mcf-color-field1').val()){
-			$('.mcf-color-field1').val('#FFFFFF');
-		}
-        $('.mcf-color-picker1').fadeIn();
-    });
-	$('#mcf_section_icon_color').click(function() {
-		if(!$('.mcf-color-field2').val()){
-			$('.mcf-color-field2').val('#FFFFFF')
-		}
-        $('.mcf-color-picker2').fadeIn();
-    });
-
-    $(document).mousedown(function() {
-        $('#mcf-colorpicker1').each(function() {
-            var display = $(this).css('display');
-            if ( display == 'block' )
-                $(this).fadeOut();
-        });
-    });
-	
-	$(document).mousedown(function() {
-        $('#mcf-colorpicker2').each(function() {
-            var display = $(this).css('display');
-            if ( display == 'block' )
-                $(this).fadeOut();
-        });
-    });
+jQuery(document).ready(function($){
+    $('.mcf-color-field1').wpColorPicker();
+	$('.mcf-color-field2').wpColorPicker();
 });
+
